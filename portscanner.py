4f9b2ac6ebb35142f66893d82f6afa5ab0ba6424
thetonyharkness <87252675+thetonyharkness@users.noreply.github.com>
2023-08-23 13:24:58 -0500

    added addtl. function
    added display_open_ports function to separate each task the program conducts

--- portscanner.py
+++ portscanner.py
@@ -8,44 +8,42 @@ import pyfiglet
 def scan_ports(target, start_port, end_port):
     open_ports = []
     for port in range(start_port, end_port + 1):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
-        result = sock.connect_ex((target, port))
-        if result == 0:
-            open_ports.append(port)
-        sock.close()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(1)
+            result = sock.connect_ex((target, port))
+            if result == 0:
+                open_ports.append(port)
     return open_ports
 
+def display_open_ports(ip, ports):
+    if ports:
+        print(f"\n Open ports for {ip}:")
+        for port in ports:
+            print(termcolor.colored(f"[*]   Port {port} is open", 'green'))
+    else:
+        print(f"No open ports found for {ip}.")
+
 def main():
-    ascii_banner = pyfiglet.figlet_format("tony's port scanner")
+    ascii_banner = pyfiglet.figlet_format("Tony's Port Scanner")
     print(ascii_banner)
     
-    targets = input("Enter the target(s) to scan: ")
-    target_list = targets.split(',')
-    
+    targets = input("Enter the target(s) to scan (comma-separated & CIDR accepted): ")
+    target_list = [target.strip() for target in targets.split(',')]
+
     start_port = int(input("Enter the starting port: "))
     end_port = int(input("Enter the ending port: "))
     
     for target in target_list:
-        target = target.strip()
         print(f"\nScanning ports for {target}...")
         
         if '/' in target:  # CIDR range
             ip_range = ipaddress.IPv4Network(target, strict=False)
             for ip in ip_range.hosts():
                 open_ports = scan_ports(str(ip), start_port, end_port)
-                if open_ports:
-                    print(f"\n Open ports for {ip}:")
-                    for port in open_ports:
-                        print(termcolor.colored(f"[*]   Port {port} is open", 'green'))
+                display_open_ports(ip, open_ports)
         else:  # Single host
             open_ports = scan_ports(target, start_port, end_port)
-            if open_ports:
-                print("\n[*] Open ports:")
-                for port in open_ports:
-                    print(termcolor.colored(f"[*]   Port {port} is open", 'green'))
-            else:
-                print(f"No open ports found for {target}.")
+            display_open_ports(target, open_ports)
 
     scan_another = input("\nDo you want to scan another target? (yes/no): ")
     if scan_another.lower() == "yes":
